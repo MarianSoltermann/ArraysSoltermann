4d4e325203b2f2d7dfb773fdb80bcaa11297435f
MarianSoltermann <mariansoltermann@gmail.com>
2022-09-22 20:41:45 -0300

    agregué productos

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,22 +1,23 @@
 
 //pregunta de edad
-let edad = parseInt(prompt("Ingrese su edad"));
+/*let edad = parseInt(prompt("Ingrese su edad"));
 if(edad < 18){
   alert("Sos menor de edad. Prohibida la venta");
 }else{
   alert("Puedes beber alcohol con moderación");
 }
 
+
 //stock de productos
 const productos = [
     { id: 1, articulo: "Gin", marca: "Bombay", precio: 2500 },
     { id: 2, articulo: "Gin", marca: "Tanqueray", precio: 3000 },
-    { id: 3, articulo: "Vino", marca: "Trapiche", precio: 1200 },
+    { id: 3, articulo: "Vino", marca: "Trumpeter", precio: 1200 },
     { id: 4, articulo: "Vino", marca: "Santa Julia", precio: 800 },
     { id: 5, articulo: "Fernet", marca: "Branca", precio: 2000 },
     { id: 6, articulo: "Vodka", marca: "Absolut", precio: 1800 },
-    { id: 7, articulo: "Whisky", marca: "Jack Daniels", precio: 35000 },
-    { id: 8, articulo: "Cerveza", marca: "Andes", precio: 400 },
+    { id: 7, articulo: "Whisky", marca: "Jim Beam", precio: 35000 },
+    { id: 8, articulo: "Cerveza", marca: "Stella", precio: 400 },
     { id: 9, articulo: "Cerveza", marca: "Patagonia", precio: 500 },
 ];
 
@@ -70,3 +71,4 @@ function mostrarTotal() {
 ingresarPrecio();
 calcularTotal();
 mostrarTotal();
+*/
